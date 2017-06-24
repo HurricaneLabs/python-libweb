33ae64f44c8d1003041962a4ae1a93bfa758a45c
Steve McMaster <mcmaster@hurricanelabs.com>
2017-06-24 10:49:58 -0400

    Spamhaus DNSBL requests fail when sent through 8.8.8.8 / 8.8.4.4 apparently...
    * Update DNS tests to use *.testing.machinae-app.io instead of *.spamhaus.org
    * Add additional testing to confirm that tests pass when run against Google DNS

--- tests/test_dns.py
+++ tests/test_dns.py
@@ -3,6 +3,21 @@ import unittest
 from libweb.dns import DnsblService, DnsService
 
 
+class GoogleDnsMixin:
+    def get_resolver(self):
+        resolver = super().get_resolver()
+        resolver.nameservers = ["8.8.8.8", "8.8.4.4"]
+        return resolver
+
+
+class GoogleDnsService(GoogleDnsMixin, DnsService):
+    pass
+
+
+class GoogleDnsblService(GoogleDnsMixin, DnsblService):
+    pass
+
+
 class TestDns(unittest.TestCase):
     def setUp(self):
         self.service = DnsService()
@@ -13,7 +28,7 @@ class TestDns(unittest.TestCase):
 
     def test_single_dns_request(self):
         self.service.conf = {
-            "rrname": "test.dbl.spamhaus.org",
+            "rrname": "test.dbl.testing.machinae-app.io",
             "rrtype": "A"
         }
 
@@ -25,8 +40,8 @@ class TestDns(unittest.TestCase):
 
     def test_multiple_dns_requests(self):
         self.service.conf = [
-            {"rrname": "test.dbl.spamhaus.org", "rrtype": "A"},
-            {"rrname": "dbltest.com.dbl.spamhaus.org", "rrtype": "A"}
+            {"rrname": "test.dbl.testing.machinae-app.io", "rrtype": "A"},
+            {"rrname": "dbltest.com.dbl.testing.machinae-app.io", "rrtype": "A"}
         ]
 
         for data in self.service:
@@ -35,7 +50,7 @@ class TestDns(unittest.TestCase):
 
     def test_nxdomain_stops_iteration(self):
         self.service.conf = {
-            "rrname": "example.com.dbl.spamhaus.org",
+            "rrname": "example.com.dbl.testing.machinae-app.io",
             "rrtype": "A"
         }
 
@@ -43,7 +58,7 @@ class TestDns(unittest.TestCase):
 
     def test_txt_strip_quotes(self):
         self.service.conf = {
-            "rrname": "dbltest.com.dbl.spamhaus.org",
+            "rrname": "dbltest.com.dbl.testing.machinae-app.io",
             "rrtype": "TXT"
         }
 
@@ -55,7 +70,7 @@ class TestDns(unittest.TestCase):
 
     def test_txt_split(self):
         self.service.conf = {
-            "rrname": "dbltest.com.dbl.spamhaus.org",
+            "rrname": "dbltest.com.dbl.testing.machinae-app.io",
             "rrtype": "TXT",
             "split": "/"
         }
@@ -67,7 +82,7 @@ class TestDns(unittest.TestCase):
 
     def test_dns_request_opts(self):
         self.service.conf = {
-            "rrname": "{target}.dbl.spamhaus.org",
+            "rrname": "{target}.dbl.testing.machinae-app.io",
             "rrtype": "A"
         }
         self.service.opts = {
@@ -81,16 +96,23 @@ class TestDns(unittest.TestCase):
         self.assertEqual(set(rdata), set(["127.0.1.2"]))
 
 
+class TestDnsGoogleResolver(TestDns):
+    def setUp(self):
+        self.service = GoogleDnsService()
+        self.service.swallow_exceptions = False
+
+
 class TestDnsbl(unittest.TestCase):
     def setUp(self):
         self.service = DnsblService()
+        self.service.swallow_exceptions = False
 
     def tearDown(self):
         pass
 
     def test_dnsbl_request(self):
         self.service.conf = {
-            "rrname": "{target}.zen.spamhaus.org",
+            "rrname": "{target}.zen.testing.machinae-app.io",
             "rrtype": "A"
         }
         self.service.opts = {
@@ -102,3 +124,9 @@ class TestDnsbl(unittest.TestCase):
             self.assertIn("rdata", data)
             rdata.append(data["rdata"])
         self.assertEqual(set(rdata), set(["127.0.0.10", "127.0.0.2", "127.0.0.4"]))
+
+
+class TestDnsblGoogleResolver(TestDnsbl):
+    def setUp(self):
+        self.service = GoogleDnsblService()
+        self.service.swallow_exceptions = False
